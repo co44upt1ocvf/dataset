2f1fdcd2e298e4c617b4c1ec2b4c05fdfc1c0e23
co44upt1ocvt <corruold@gmail.com>
2023-12-17 19:55:10 +0300

    i wanna cry

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import pandas as pd
+import numpy as np
 
+pd.set_option('display.max_columns', None)
+anime = pd.read_csv("anime.csv")
+
+print(anime)
+print(f"anime.ndim = {anime.ndim}")
+print(anime.columns)
+print(anime["Episodes"].unique())
+print(anime.sort_values(by='Episodes'))
